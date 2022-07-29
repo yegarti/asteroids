65bf3108f18fb326e8637c0d38bbe75affeb388a
Yehuda Garti <yeudag@gmail.com>
2022-07-29 14:38:03 +0300

    Added player 1axis movement

--- asteroids/actor.py
+++ asteroids/actor.py
@@ -1,28 +1,38 @@
-import operator
-from dataclasses import dataclass, field
-
 import pygame as pg
+from pygame.math import Vector2
 import pygame.transform
 
 
-@dataclass
-class Actor(pg.sprite.Sprite):
-    image: pg.Surface
-    position: tuple[int, int]
-    velocity: tuple[float, float]
-    angle: float = 0
-    rect: pg.Rect = field(init=False)
+# @dataclass(unsafe_hash=True)
+from asteroids.utils import load_image
 
-    def __post_init__(self):
-        # self.rect.center =
+
+class Actor(pg.sprite.Sprite):
+    def __init__(self, image_name, scale=1.):
+        super().__init__()
+        self._original_image = load_image(image_name)
+        self._scale(scale)
+        self.image = self._original_image.copy()
+        self.velocity = Vector2(0, 0)
+        self.position = Vector2(0, 0)
+        self.angle = 0
+
+    @property
+    def image(self):
+        return self._image
+
+    @image.setter
+    def image(self, value):
+        self._image = value
         self.rect = self.image.get_rect()
 
-    def move(self, x, y):
-        self.rect.move_ip(x, y)
+    def _scale(self, factor):
+        self._original_image = pg.transform.scale(
+            self._original_image,
+            (factor * self._original_image.get_width(),
+             factor * self._original_image.get_height()))
 
-    def rotate(self, angle):
-        self.angle += angle
-
-    def render(self):
-        image = pygame.transform.rotate(self.image, self.angle)
-        return image, image.get_rect()
+    def update(self, dt) -> None:
+        self.position += self.velocity * dt
+        self.rect.x = self.position.x
+        self.rect.y = self.position.y
--- asteroids/asteroids.py
+++ asteroids/asteroids.py
@@ -3,12 +3,14 @@ import pygame.display
 from pygame.locals import *
 
 from asteroids.actor import Actor
+from asteroids.player import Player
 from asteroids.utils import load_image, repeat_surface
 
 
 class Asteroids:
     MOVEMENT_SCALAR = 0.25
     ANGLE = 0.1
+    PLAYER_SCALE = 0.5
 
     def __init__(self, width, height, title='Asteroids'):
         pg.display.set_caption(title)
@@ -17,12 +19,14 @@ class Asteroids:
         self._clock = pygame.time.Clock()
         self.background = repeat_surface(self.screen.get_size(),
                                          load_image('purple'))
+        self.player = Player()
+        # self._player: Actor = Actor(image=img,
+        #                             position=self._get_center(),
+        #                             velocity=(0, 0))
+        self.all_actors = pygame.sprite.Group()
+        self.all_actors.add(self.player)
+        # self.all_actors.draw()
 
-        img = pg.Surface((20, 40))
-        img.fill((255, 0, 0))
-        self._player: Actor = Actor(image=img,
-                                    position=self._get_center(),
-                                    velocity=(0, 0))
         self.delta = 0
 
     def _get_center(self):
@@ -38,26 +42,29 @@ class Asteroids:
     def _update_player(self):
         keys = pg.key.get_pressed()
         # if keys[K_d]:
-        #     self._player.rotate(-self.ANGLE * self.delta)
+        #     self.player.rotate_cw(self.delta)
         # if keys[K_a]:
-        #     self._player.rotate(self.ANGLE * self.delta)
-        # if keys[K_s]:
-        #     self._player.move(0, self.MOVEMENT_SCALAR * self.delta)
-        # if keys[K_w]:
-        #     self._player.move(0, -self.MOVEMENT_SCALAR * self.delta)
+        #     self.player.rotate_ccw(self.delta)
+        if keys[K_s]:
+            self.player.velocity.y += 0.01
+        if keys[K_w]:
+            self.player.velocity.y -= 0.01
         pass
 
     def update(self):
         dt = self._clock.tick(60)
         self.delta = dt
+        self.all_actors.update(dt)
         self._handle_events()
         self._update_player()
         # self.screen.blit(pg.Surface(rect.size), (0, 0))
 
     def render(self):
         self.screen.blit(self.background, (0, 0))
+        # self.screen.blit(self.player, (0, 0))
+        self.all_actors.draw(self.screen)
         # self.screen.fill((0, 0, 0))
-        image, rect = self._player.render()
+        # image, rect = self._player.render()
         # self.screen.blit(self._player.image, self._player.rect)
-        self.screen.blit(image, rect)
+        # self.screen.blit(image, rect)
         pg.display.flip()
